f1929bbd94e139632c352149f9856fe46c597d1f
chingchinghuang <huang27c@mtholyoke.edu>
2019-03-11 12:02:32 -0400

    Add game class

---
+++ game.py
@@ -0,0 +1,22 @@
+#Game class provides the grid for prisoners dilemma.
+
+class Game:
+
+	grid = []
+
+	'''
+	Construct a game by takes in payoffs for each cell
+	Parameter:  v1, v2, v3, v4 are vectors that contain payoffs of both players.
+	'''
+	def __init__(self, v1, v2, v3, v4):
+		self.grid = [[v1, v2],[v3, v4]]
+
+
+	'''
+	Returns the payoffs for both player from their decisions
+	Parameter: decisions from both player
+	Return: a list size of two with both players' payoffs
+	'''
+	def getPayOffs(self, decision1, decision2):
+		return self.grid[decision1][decision2]
+
